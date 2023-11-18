83577aa27261a621113617a63214fd425b2bd5f5
YolsonDev <yolsondev@gmail.com>
2023-11-18 18:22:45 +0100

    Initial commit

---
+++ src/hello.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Sebastian Kowalewski");
+        System.out.println("Album: 30511");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Podaj swoje imie: ");
+
+        String wprowadzonyTekst = scanner.nextLine();
+
+        System.out.println("");
+    }
+}
